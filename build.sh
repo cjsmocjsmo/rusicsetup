b188c8855ea02d3bd815b1c59788175e432248c7
Charlie J Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2026-06-18 22:43:54 -0700

    chore: Update .gitignore and build script for ARM binaries; remove old binary files

--- build.sh
+++ build.sh
@@ -12,6 +12,7 @@ ARM32=./target/armv7-unknown-linux-gnueabihf/release/rusicsetup
 if [ -f "$ARM64" ]; then
     echo "Copying ARM64 binary to current directory..."
     cp "$ARM64" ./rusicsetup-rpi4-"$1"
+    cp "$ARM64" ../rusic/setup/rusicsetup-rpi4-"$1"
 else
     echo "ARM64 binary not found. Please ensure it was built successfully."
 fi
@@ -19,6 +20,7 @@ fi
 if [ -f "$ARM32" ]; then
     echo "Copying ARM32 binary to current directory..."
     cp "$ARM32" ./rusicsetup-rpi3b-"$1"
+    cp "$ARM32" ../rusic/setup/rusicsetup-rpi3b-"$1"
 else
     echo "ARM32 binary not found. Please ensure it was built successfully."
 fi
